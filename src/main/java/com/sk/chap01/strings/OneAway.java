c7b425b83874b3bb7b8bd5344fe7516ad138beec
Srikanth Kambhampati <skambhampati@homeaway.com>
2019-12-28 23:42:23 -0600

    Chapter 01

--- src/main/java/com/sk/chap01/strings/OneAway.java
+++ src/main/java/com/sk/chap01/strings/OneAway.java
@@ -1,5 +1,9 @@
 package com.sk.chap01.strings;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * 1.5 There are three types of edits that can be performed on strings:
  * insert a character,
@@ -8,4 +12,54 @@ package com.sk.chap01.strings;
  * Given two strings, write a function to check if they are one edit (or zero edits) away
  */
 public class OneAway {
+    private static boolean isOneEditAway(String source, String target) {
+        if (source.length() == target.length()) {
+            return isOneCharReplace(source, target);
+        } else if (source.length() == target.length() + 1) {
+            return isOneCharRemove(source, target);
+        } else if (source.length() + 1 == target.length()) {
+            return isOneCharRemove(target, source);
+        }
+        return false;
+    }
+
+    private static boolean isOneCharRemove(String input1, String input2) {
+        Map<Character, Long> input1CountMap = getCharCount(input1);
+
+        for (char ch : input2.toCharArray()) {
+            input1CountMap.computeIfPresent(ch, (k,v) -> v = v -1);
+        }
+
+        return input1CountMap.entrySet().stream().filter(e -> e.getValue() == 1).count() == 1;
+    }
+
+    private static Map<Character, Long> getCharCount(String input) {
+        return input.chars()
+            .mapToObj(i -> (char) i)
+            .map(Character::toLowerCase)
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private static boolean isOneCharReplace(String source, String target) {
+        int replacedCount = 0;
+        for (int i = 0; i < source.length(); i++) {
+            if (source.charAt(i) != target.charAt(i)) {
+                replacedCount++;
+            }
+            if (replacedCount > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOneEditAway("pale", "ple"));
+        System.out.println(isOneEditAway("pale", "pales"));
+        System.out.println(isOneEditAway("pale", "bale"));
+        System.out.println(isOneEditAway("pale", "bae"));
+        System.out.println(isOneEditAway("tales", "pale"));
+        System.out.println(isOneEditAway("ales", "pale"));
+        System.out.println(isOneEditAway("sale", "pale"));
+    }
 }
---
+++ src/main/java/com/sk/chap01/strings/StringCompression.java
@@ -0,0 +1,35 @@
+package com.sk.chap01.strings;
+
+/**
+ * 1.6 Implement a method to perform basic string compression using the counts of repeated characters.
+ * For example, the string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not become smaller
+ * than the original string, your method should return the original string. You can assume the string has only
+ * uppercase and lowercase letters (a-z)
+ */
+public class StringCompression {
+    private static String compressString(String input) {
+        int count = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length() - 1; i ++) {
+            if (input.charAt(i) != input.charAt(i + 1)) {
+                sb.append(input.charAt(i)).append(count);
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        sb.append(input.charAt(input.length() - 1)).append(count); //add the last char count as well
+
+        if (sb.length() > input.length()) {
+            return input;
+        } else {
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compressString("aabcccccaaa"));
+        System.out.println(compressString("abcdefgh"));
+        System.out.println(compressString("aabccccca"));
+    }
+}
