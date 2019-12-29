d8d49738b406cf082c3aacd56aeea81f82cda5f4
Srikanth Kambhampati <skambhampati@homeaway.com>
2019-12-28 23:55:57 -0600

    Chapter 01

---
+++ src/main/java/com/sk/chap01/strings/RotateMatrix.java
@@ -0,0 +1,8 @@
+package com.sk.chap01.strings;
+
+/**
+ * 1.7 Given an image represented by NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image
+ * by 90 degrees. Can you do this in place?
+ */
+public class RotateMatrix {
+}
---
+++ src/main/java/com/sk/chap01/strings/StringRotation.java
@@ -0,0 +1,29 @@
+package com.sk.chap01.strings;
+
+/**
+ * 1.9 Assume you have a method isSubstring which checks if one word is a substring of another.
+ * Given two strings s1 and s2, write code to check if s2 is a rotation of s1 using only one call
+ * to isSubstring
+ */
+public class StringRotation {
+    private static boolean isRotation(String s1, String s2) {
+        int length = s1.length();
+        if (length == 0 || length != s2.length()) {
+            return false;
+        }
+
+        String s1s1 = s1 + s1;
+
+        return isSubstring(s1s1, s2);
+    }
+
+    private static boolean isSubstring(String source, String target) {
+        return source.contains(target);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isRotation("waterbottle", "erbottlewat"));
+        System.out.println(isRotation("waterbottle", "abcd"));
+        System.out.println(isRotation("cdab", "abcd"));
+    }
+}
